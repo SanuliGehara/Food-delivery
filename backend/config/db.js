456cf64ad4c94d510d3ec14a8708cc6ddd259dcf
SanuliGehara <geharasanuli5@gmail.com>
2024-07-09 11:36:56 +0530

    update sceret keys

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://sanuligehara:20051207@cluster0.s8capwx.mongodb.net/Food-Delivery"
-    )
-    .then(() => console.log("DB connected"));
+  // replace with your database connection link
+  await mongoose.connect("#").then(() => console.log("DB connected"));
 };
